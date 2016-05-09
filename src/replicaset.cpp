d313bad1bff745c51e259c5ea5ca25a7db4a4e80
David Kimura <dkimura@pivotal.io>
2016-05-08 19:58:18 -0700

    Add empty replicaset class

---
+++ src/replicaset.cpp
@@ -0,0 +1,13 @@
+#include <replicaset.hpp>
+
+
+void
+ReplicaSet::Add(Replica replica)
+{
+}
+
+
+void
+ReplicaSet::Remove(Replica replica)
+{
+}
---
+++ src/replicaset.hpp
@@ -0,0 +1,8 @@
+#include <replica.hpp>
+
+
+class ReplicaSet
+{
+    void Add(Replica replica);
+    void Remove(Replica replica);
+};
