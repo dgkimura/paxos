1d96becad047119abd6983a05642b4cf29bb2edf
David Kimura <dkimura@pivotal.io>
2016-05-14 09:04:22 -0700

    Add empty sender methods

--- src/sender.cpp
+++ src/sender.cpp
@@ -1,1 +1,11 @@
 #include <sender.hpp>
+
+
+Sender::Sender()
+{
+}
+
+
+Sender::~Sender()
+{
+}
--- src/sender.hpp
+++ src/sender.hpp
@@ -1,3 +1,20 @@
+#include <messages.hpp>
+
 class Sender
 {
+public:
+
+    Sender();
+
+    ~Sender();
+
+    template <typename T>
+    void Reply(Message message)
+    {
+    }
+
+    template <typename T>
+    void ReplyAll()
+    {
+    }
 };
