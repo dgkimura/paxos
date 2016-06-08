f1be79fcb6a966e301b93c2ba01582e6aeb2297b
David Kimura <dkimura@pivotal.io>
2016-06-08 11:38:59 -0700

    Add callback bind unittest

--- unittests/callback_unittest.cpp
+++ unittests/callback_unittest.cpp
@@ -15,3 +15,17 @@ TEST(CallbackTest, testCallbackWithLambdaExpression)
 
     ASSERT_TRUE(ran_callback);
 }
+
+
+TEST(CallbackTest, testCallbackWithBindExpression)
+{
+    bool ran_callback = false;
+    Message m;
+
+    using namespace std::placeholders;
+
+    Callback callback(std::bind([&](Message m, int a_number) { ran_callback = true; }, _1, 1234));
+    callback(m);
+
+    ASSERT_TRUE(ran_callback);
+}
