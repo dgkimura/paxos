825d66ec01f78bff1473920e9c1d80fceade63d7
David Kimura <dkimura@pivotal.io>
2016-08-08 17:31:39 -0700

    Add comments to message types

--- src/messages.hpp
+++ src/messages.hpp
@@ -7,12 +7,39 @@
 
 enum class MessageType
 {
+    //
+    // RequestMessage sent when there is a new proposal.
+    //
     RequestMessage,
+
+    //
+    // PrepareMessage sent to signal beginning of a round of paxos.
+    //
     PrepareMessage,
+
+    //
+    // PromiseMessage sent to acknowledge a prepare.
+    //
     PromiseMessage,
+
+    //
+    // AcceptMessage sent if quorum has elected a leader.
+    //
     AcceptMessage,
+
+    //
+    // AcceptedMessage sent to prepare for a new round of paxos.
+    //
     AcceptedMessage,
+
+    //
+    // UpdateMessage sent when replica realizes it is behind.
+    //
     UpdateMessage,
+
+    //
+    // UpdatedMessage sent to update a replica that has fallen behind.
+    //
     UpdatedMessage
 };
 
