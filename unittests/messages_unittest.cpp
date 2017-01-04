b8b3de9804081960593b05911c367c045b39190a
David Kimura <dkimura@pivotal.io>
2017-01-04 13:13:02 -0800

    Add unittests for message type hash

--- unittests/messages_unittest.cpp
+++ unittests/messages_unittest.cpp
@@ -17,3 +17,18 @@ TEST(MessageTest, testMessageResponseUpdatesSenderAndReceiver)
     ASSERT_EQ(response.to.hostname, m.from.hostname);
     ASSERT_EQ(response.type, MessageType::PromiseMessage);
 }
+
+
+TEST(MessageTest, testMessageTypeHashFunction)
+{
+    ASSERT_EQ(0, MessageTypeHash()(MessageType::RequestMessage));
+    ASSERT_EQ(1, MessageTypeHash()(MessageType::RetryRequestMessage));
+    ASSERT_EQ(2, MessageTypeHash()(MessageType::PrepareMessage));
+    ASSERT_EQ(3, MessageTypeHash()(MessageType::RetryPrepareMessage));
+    ASSERT_EQ(4, MessageTypeHash()(MessageType::PromiseMessage));
+    ASSERT_EQ(5, MessageTypeHash()(MessageType::NackMessage));
+    ASSERT_EQ(6, MessageTypeHash()(MessageType::AcceptMessage));
+    ASSERT_EQ(7, MessageTypeHash()(MessageType::AcceptedMessage));
+    ASSERT_EQ(8, MessageTypeHash()(MessageType::UpdateMessage));
+    ASSERT_EQ(9, MessageTypeHash()(MessageType::UpdatedMessage));
+}
