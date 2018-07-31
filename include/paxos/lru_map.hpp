98708aa2b4e216c470bfa75513b96be6045ff80c
David Kimura <dkimura@pivotal.io>
2018-07-31 12:54:00 -0700

    Add LRU map

---
+++ include/paxos/lru_map.hpp
@@ -0,0 +1,89 @@
+#ifndef __LRU_MAP_HPP_INCLUDED__
+#define __LRU_MAP_HPP_INCLUDED__
+
+#include <deque>
+#include <functional>
+#include <map>
+
+
+namespace paxos
+{
+
+
+template <class Key, class T, class Compare=std::less<Key>>
+class lru_map
+{
+public:
+
+    using map_type = std::map<Key, T>;
+    using iterator = typename map_type::iterator;
+
+    lru_map(size_t capacity)
+        : capacity(capacity)
+    {
+    }
+
+    T&
+    operator[](const Key e)
+    {
+        if (map.find(e) != map.end())
+        {
+            return map[e];
+        }
+
+        queue.push_back(e);
+        if (queue.size() > capacity)
+        {
+            // remove
+            auto front = queue[0];
+            queue.pop_front();
+            map.erase(front);
+        }
+        return map[e];
+    }
+
+    iterator
+    find(const Key e)
+    {
+        return map.find(e);
+    }
+
+    void
+    erase(const Key e)
+    {
+        map.erase(e);
+    }
+
+    iterator
+    begin()
+    {
+        return map.begin();
+    }
+
+    iterator
+    end()
+    {
+        return map.end();
+    }
+
+    size_t
+    size() const
+    {
+        return map.size();
+    }
+
+private:
+
+    size_t capacity;
+
+    std::map<Key, T, Compare> map;
+
+    std::deque<Key> queue;
+};
+
+
+}
+
+
+#endif
+
---
+++ unittests/lru_map_unittest.cpp
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+
+#include "paxos/lru_map.hpp"
+
+
+TEST(LruMapTest, testContainsInsertedElement)
+{
+    paxos::lru_map<int, int> lruset(10);
+
+    lruset[3] = 42;
+    ASSERT_EQ(42, lruset[3]);
+}
+
+
+TEST(LruMapTest, testInsertExceedingCapacityRemovesLeastRecentlyUsedElement)
+{
+    paxos::lru_map<int, int> lruset(5);
+
+    lruset[1] = 1;
+    lruset[2] = 2;
+    lruset[3] = 3;
+    lruset[4] = 4;
+    lruset[5] = 5;
+    lruset[6] = 6;
+
+    // First element should be evicted
+    ASSERT_EQ(lruset.end(), lruset.find(1));
+    ASSERT_NE(lruset.end(), lruset.find(2));
+    ASSERT_NE(lruset.end(), lruset.find(3));
+    ASSERT_NE(lruset.end(), lruset.find(4));
+    ASSERT_NE(lruset.end(), lruset.find(5));
+    ASSERT_NE(lruset.end(), lruset.find(6));
+}
+
+
+TEST(LruMapTest, testInsertSameElementDoesNotEvictElements)
+{
+    paxos::lru_map<int, int> lruset(2);
+
+    lruset[1] = 1;
+    lruset[2] = 2;
+    lruset[2] = 2;
+    ASSERT_NE(lruset.end(), lruset.find(1));
+}
